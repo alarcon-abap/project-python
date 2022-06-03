f81bbb6c82af2236cdcc37453c28dbc280d5cb21
Rodrigo de O. Alarcon <oliveira.alarcon@gmail.com>
2022-06-03 18:55:35 -0300

    Update calcDec2Bin.py

--- calculadoraDec2Bin/calcDec2Bin.py
+++ calculadoraDec2Bin/calcDec2Bin.py
@@ -4,11 +4,14 @@ guardaValor = int(valorEntrada)
 resultado = ""
 
 while valorEntrada > 0:
-  if (valorEntrada % 2) == 0:
-    resultado = "0" + resultado
-  else:
-    resultado = "1" + resultado
-  
-  valorEntrada = valorEntrada // 2
+    try:
+        if (valorEntrada % 2) == 0:
+            resultado = "0" + resultado
+        else:
+            resultado = "1" + resultado
+
+        valorEntrada = valorEntrada // 2
+    except:
+        print("Você digitou um texto, digite um número")
 
 print(f'Você Digitou o número inteiro: {guardaValor} \nO Equivalente em Binário é: {resultado}')
